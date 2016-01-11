06205e52518cd8875627337f688ba569cade9b95
Juho Vepsalainen <bebraw@gmail.com>
2016-01-11 20:50:36 +0200

    Set up `.webpackrc` for testing

--- test.js
+++ test.js
@@ -2,6 +2,10 @@
 const assert = require('assert');
 const webpackPresets = require('./lib');
 
+const webpackrc = JSON.parse(fs.readFileSync('./.webpackrc', {
+  encoding: 'utf-8'
+}));
+
 describe('Presets', function () {
   it('should be able to evaluate', function () {
     // TODO
